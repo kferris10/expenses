eedd8d843548540a2a91c5b2b314cc58dd6296e9
kferris10 <kevin.ferris10@gmail.com>
2017-04-16 21:20:18 -0400

    2017 update

--- data-cleaning.R
+++ data-cleaning.R
@@ -5,8 +5,6 @@ library(ggplot2)
 library(ggalt)
 library(lubridate)
 library(plotly)
-library(ggiraph)
-library(streamgraph)
 
 exp <- gs_title("Purchases")
 # re-register
@@ -30,29 +28,37 @@ by_date <- dat %>%
 qplot(Date, Amount, data = by_date, colour = Category, geom = "line")
 
 # aggregate by month, category, Reason
+month_levels <- paste(
+  rep(sort(unique(year(dat$Date))), each = 12), 
+  rep(1:12, n_distinct(year(dat$Date))), 
+  sep = "-"
+)
 by_month_category_reason <- dat %>% 
-  mutate(month = month(Date)) %>% 
-  group_by(month, Category, Reason) %>% 
+  mutate(year = year(Date), 
+         month = month(Date), 
+         ym = factor(paste(year, month, sep = "-"), 
+                     levels = month_levels)) %>% 
+  group_by(ym, Category, Reason) %>% 
   summarise(Amount = round(sum(Amount))) %>% 
   ungroup()
 
 # aggregate by month, category
 by_month_category <- by_month_category_reason %>% 
   mutate(spending = paste(Reason, Amount, sep = ": $")) %>% 
-  group_by(month, Category) %>% 
+  group_by(ym, Category) %>% 
   summarise(Amount = sum(Amount), 
             Reason = paste(spending, collapse = ", ")) %>% 
   ungroup()
 
 # calculate the total amont spent each month
 by_month <- by_month_category %>% 
-  group_by(month) %>% 
+  group_by(ym) %>% 
   arrange(Category) %>% 
   mutate(tot_Amount = cumsum(Amount)) %>% 
   ungroup()
 
 
-gg <- ggplot(by_month, aes(month, Amount)) 
+gg <- ggplot(by_month, aes(ym, Amount)) 
 # dashed line for how much I make each month
 gg <- gg + 
   geom_hline(yintercept = 1468 * 2, colour = "darkgrey", linetype = "dashed")
@@ -62,7 +68,8 @@ gg <- gg +
 gg <- gg + 
   geom_point(aes(y = tot_Amount, colour = Category, text = Reason), 
              alpha = .5) + 
-  scale_colour_brewer(type = "qual", palette = 2, guide = F)
+  scale_colour_brewer(type = "qual", palette = 2, guide = F) + 
+  scale_x_discrete(name = "Date")
 
 # shaded area plot for expenses
 gg <- gg + 
@@ -73,9 +80,10 @@ gg <- gg +
 gg <- gg + theme_bw()
 gg <- gg + theme(
   panel.grid = element_blank(), 
-  panel.border = element_blank()
+  panel.border = element_blank(), 
+  axis.text.x = element_text(angle = 45, vjust = 0.5)
 )
-# gg
+gg
 
 ggplotly(gg, tooltip = c("x", "y", "colour", "text"))
 
@@ -84,7 +92,7 @@ ggplotly(gg, tooltip = c("x", "y", "colour", "text"))
 gg2 <- ggplot(by_month, aes(Amount, Category)) + 
   geom_lollipop(horizontal = TRUE, point.colour = "steelblue", 
                 point.size = 3, alpha = .7) + 
-  facet_wrap(~month)
+  facet_wrap(~ym)
 gg2 <- gg2 + theme_bw()
 gg2 <- gg2 + theme(panel.grid.minor = element_blank(), 
                    panel.grid.major.y = element_blank())
